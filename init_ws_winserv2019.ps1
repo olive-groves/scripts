36a1a3fb7736c29cacd5fa7da748ecdc689a4edb
Marco Roling <marcoroling@hotmail.com>
2023-05-11 12:51:16 +0200

    Create init_ws_winserv2019.ps1

---
+++ init_ws_winserv2019.ps1
@@ -0,0 +1,7 @@
+## this is a powershell script to initate a workspace for a virtual windows2019 machine, to run the butterfly image viewer
+
+## first try and set this option to be able to execute stuff
+Set-ExecutionPolicy RemoteSigned
+
+## show a message at the end 
+Read-Host -Prompt "Press Enter to exit"
